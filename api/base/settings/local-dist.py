8339fd198aae665ddda39e78327ab01fb5761fa7
John Tordoff <>
2021-01-26 13:54:59 -0500

    fix django-include

--- api/base/settings/local-dist.py
+++ api/base/settings/local-dist.py
@@ -12,10 +12,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
 if DEBUG:
-    INSTALLED_APPS += ('debug_toolbar', 'nplusone.ext.django',)
+    INSTALLED_APPS += ('debug_toolbar',)
     MIDDLEWARE += (
         'debug_toolbar.middleware.DebugToolbarMiddleware',
-        'nplusone.ext.django.NPlusOneMiddleware',
     )
     DEBUG_TOOLBAR_CONFIG = {
         'SHOW_TOOLBAR_CALLBACK': lambda _: True,
